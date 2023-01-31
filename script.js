5173f968e9957b301ffb6f5905e923d0632800b8
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-01-31 20:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function clearData() {
+	document.getElementById("input").value = "";
+}
+function dis(val) {
+	document.getElementById("input").value += val;
+}
+function answer() {
+	let p=document.getElementById("input").value;
+	let q=eval(p);
+	document.getElementById("input").value=q;
+}
